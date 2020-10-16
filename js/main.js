f35870e6cb052fec0af0aed6fc64026c1e160560
André Sebastian-Bath <a.sebastian@posteo.de>
2020-10-16 10:30:05 +0200

    finish section 13

--- js/main.js
+++ js/main.js
@@ -3,9 +3,18 @@ console.log("Main.js started.")
 var Song = Backbone.Model.extend({
     initialize: function() {
         console.log("A new song has been created.")
+    },
+    defaults: {
+        genre: "Jazz"
+    },
+    validate: function(attrs) {
+        if (!attrs.title) return "Title is required"
     }
 });
 
-var song = new Song();
+var song = new Song({
+    artist: "Biles Davids",
+    publishedYear: 1959
+});
+
 console.log("song", song)
- 
