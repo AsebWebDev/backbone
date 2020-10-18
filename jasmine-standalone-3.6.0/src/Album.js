16ff5c604b5a705113357075e156f6583d7efafd
André Sebastian-Bath <a.sebastian@posteo.de>
2020-10-18 13:57:39 +0200

    create Album

---
+++ jasmine-standalone-3.6.0/src/Album.js
@@ -0,0 +1,15 @@
+let Album = Backbone.Collection.extend({
+    model: Song,
+
+    url: "/api/songs",
+
+    getPopularSong: function() {
+        if (this.length === 0) return undefined;
+
+        let sortedSongs = this.sortBy(function(song){
+            return song.get("numberOfPlays")
+        })
+
+        return sortedSongs[sortedSongss.length - 1]
+    }
+})
